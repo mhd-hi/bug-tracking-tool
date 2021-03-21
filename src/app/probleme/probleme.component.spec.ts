8c6a32e4298e3260659b9a26d27a77a45a6eb647
Mohamed Hafdi Idrissi <mhdhafdi2@gmail.com>
2021-03-20 23:38:24 -0400

    31-Premiere etape de tests unitaires(vrai = vrai)

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -24,5 +24,33 @@ describe('ProblemeComponent', () => {
    });
 
 
-   it('#1 | Zone PRÉNOM invalide avec 2 caractères ')
+   it('#1 | Zone PRÉNOM invalide avec 2 caractères', () => {
+      expect(true).toBeTruthy();
+   });
+
+  it('#2 | Zone PRÉNOM valide avec 3 caractères', () => {
+      expect(true).toBeTruthy();
+   });
+
+   it('#3 | Zone PRÉNOM valide avec 200 caractères', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('#4 | Zone PRÉNOM invalide avec aucune valeur', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('#5 | Zone PRÉNOM valide avec 10 espaces', () => {
+  expect(true).toBeTruthy();
+  });
+
+  it('#6 | Zone PRÉNOM valide avec 2 espaces et 1 caractère', () => {
+  expect(true).toBeTruthy();
+  });
+
+
+
+
+
+
 });
