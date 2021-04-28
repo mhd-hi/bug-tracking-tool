527833174b78907fd4d488314b76572c44fcccd4
Mohamed Hafdi Idrissi <mhdhafdi2@gmail.com>
2021-04-28 18:05:04 -0400

    tp15 complete et debut 16

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -4,17 +4,8 @@ import { ZonesValidator } from '../shared/longueur-minimum/longueur-minimum.comp
 import { ITypeProbleme } from './typeprobleme';
 import { TypeproblemeService } from './typeprobleme.service';
 import { AbstractControl, ValidatorFn } from '@angular/forms';
+import { emailMatcherValidator } from '../shared/email.matcher/email-matcher.component';
 
-export class emailMatcherValidator {
-  static courrielDifferents(): ValidatorFn {
-      return (c: AbstractControl): { [key: string]: boolean } | null => {
-          if (!c['controls'].courriel.value || !c['controls'].courrielConfirmation.value) {
-            return null;
-          }
-          return c['controls'].courriel.value === c['controls'].courrielConfirmation.value ? null : { match: true };
-      };
-  }   
-}
 
 @Component({
   selector: 'Inter-probleme',
@@ -40,12 +31,18 @@ export class ProblemeComponent implements OnInit {
           courrielConfirmation: [{value: '', disabled: true}],
         }),
         telephone: [{value: '', disabled: true}],
-        message: [{value: '',  disabled: true}]
+        descriptionProbleme: ['', [Validators.required, Validators.minLength(5)]],
+        noUnite: '',
+        dateProbleme: {value: Date(), disabled: true}
     });
 
     this.problemes.obtenirProblemes()
     .subscribe(cat => this.typesProbleme = cat,
        error => this.errorMessage = <any>error);
+
+       this.problemeForm.get("notifications").valueChanges
+       .subscribe(value => this.appliquerNotifications(value));
+       //ngOnINIT
   }
 
   //Ajout de la methode save()
@@ -56,15 +53,10 @@ export class ProblemeComponent implements OnInit {
     const courrielConfirmationControl = this.problemeForm.get('courrielGroup.courrielConfirmation');
     const telephoneControl = this.problemeForm.get('telephone');
     const courrielGroupControl = this.problemeForm.get("courrielGroup");
-    const messageControl = this.problemeForm.get('message');
 
     courrielControl.clearValidators();
     courrielControl.reset();
     courrielControl.disable();
-
-    messageControl.clearValidators();
-    messageControl.reset();
-    messageControl.disable();
 
     courrielConfirmationControl.clearValidators();
     courrielConfirmationControl.reset();
@@ -79,35 +71,14 @@ export class ProblemeComponent implements OnInit {
       courrielControl.setValidators([Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+'), Validators.required]);  
       courrielConfirmationControl.enable(); 
       courrielConfirmationControl.setValidators([Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+'), Validators.required]);
-      courrielGroupControl.setValidators([Validators.compose([emailMatcherValidator.courrielDifferents()])]);
-      telephoneControl.setValidators([Validators.required])
+      courrielGroupControl.setValidators([Validators.required, Validators.compose([emailMatcherValidator.courrielDifferents()])]);
       telephoneControl.disable();
-    } else if (typeNotification === 'ParTelephone'){
+    }  else if (typeNotification === 'ParMessage'){
       telephoneControl.enable(); 
-      telephoneControl.setValidators([Validators.required]);
-    } else if (typeNotification === 'ParMessage'){
-      telephoneControl.enable(); 
-      telephoneControl.setValidators([ Validators.minLength(10), Validators.maxLength(10), Validators.required]);
-      messageControl.enable();
-      messageControl.setValidators([Validators.required]);
-      messageControl.setValidators([Validators.pattern('[0-9]+'), Validators.required]);  
-    }
-    else
-    {
-      courrielControl.disable();
-      courrielControl.reset();  
-      courrielConfirmationControl.disable();
-      courrielConfirmationControl.reset();  
-      telephoneControl.disable();
-      telephoneControl.reset();       
-      courrielGroupControl.disable();
-      courrielGroupControl.reset();
-      messageControl.disable();
-      messageControl.reset();
+      telephoneControl.setValidators([Validators.pattern('[0-9]+'), Validators.minLength(10), Validators.maxLength(10), Validators.required]);
     }
     courrielControl.updateValueAndValidity();
     telephoneControl.updateValueAndValidity();
     courrielConfirmationControl.updateValueAndValidity();
-    messageControl.updateValueAndValidity();
   }
 }
